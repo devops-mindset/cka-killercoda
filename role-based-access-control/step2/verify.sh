b778a5941ade111f9162d8bb5a138153e1bfdf28
Javier Canizalez <javier-canizalez@outlook.com>
2023-04-05 14:38:29 -0600

    Update verify.sh

--- role-based-access-control/step2/verify.sh
+++ role-based-access-control/step2/verify.sh
@@ -1,2 +1,1 @@
-kubectl auth can-i get pods --as=system:serviceaccount:webapp:webapp-svc -n webapp
-kubectl auth can-i list pods --as=system:serviceaccount:webapp:webapp-svc -n webapp
+kubectl auth can-i get pods --as=system:serviceaccount:webapp:webapp-svc -n webapp && kubectl auth can-i list pods --as=system:serviceaccount:webapp:webapp-svc -n webapp
