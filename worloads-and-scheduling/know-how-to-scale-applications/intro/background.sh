05912aa20c1595eba65a5322e7d728eaf198a7b3
jcanizalez <javier-canizalez@outlook.com>
2023-04-05 16:34:37 -0600

    maria

--- worloads-and-scheduling/know-how-to-scale-applications/intro/background.sh
+++ worloads-and-scheduling/know-how-to-scale-applications/intro/background.sh
@@ -1,5 +1,3 @@
 kubectl create namespace sales
 kubectl create deployment api  --image=nginx --replicas=2 -n sales
-kubectl apply -f https://k8s.io/examples/application/mysql/mysql-configmap.yaml -n sales
-kubectl apply -f https://k8s.io/examples/application/mysql/mysql-services.yaml -n sales
-kubectl apply -f https://k8s.io/examples/application/mysql/mysql-statefulset.yaml -n sales
+kubectl apply -f https://raw.githubusercontent.com/MariaDB/mariadb.org-tools/master/anel/k8s/mariadb/mariadb-sts.yaml
