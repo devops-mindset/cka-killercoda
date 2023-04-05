195ce7cbde30110f9d3c815b9b817f0d833ae937
jcanizalez <javier-canizalez@outlook.com>
2023-04-05 16:47:58 -0600

    fix db

--- worloads-and-scheduling/know-how-to-scale-applications/intro/background.sh
+++ worloads-and-scheduling/know-how-to-scale-applications/intro/background.sh
@@ -1,4 +1,4 @@
 kubectl create namespace sales
 kubectl create deployment api  --image=nginx --replicas=2 -n sales
-kubectl apply -f https://raw.githubusercontent.com/MariaDB/mariadb.org-tools/master/anel/k8s/mariadb/mariadb-secret.yaml -n sales
-kubectl apply -f https://raw.githubusercontent.com/MariaDB/mariadb.org-tools/master/anel/k8s/mariadb/mariadb-sts.yaml -n sales
+kubectl apply -f https://raw.githubusercontent.com/MariaDB/mariadb.org-tools/master/anel/k8s/mariadb/mariadb-secret.yaml -n db
+kubectl apply -f https://raw.githubusercontent.com/MariaDB/mariadb.org-tools/master/anel/k8s/mariadb/mariadb-sts.yaml -n db
